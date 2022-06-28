52b215cfe451c6f12323f90e1f4c2020c8ad0e2a
thecodingcookery <93952418+thecodingcookery@users.noreply.github.com>
2022-06-28 12:17:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function games() {
+	window.location.href = "https://xyzcoder.itch.io/";
+}
+function oldSite() {
+	window.location.href = "https://kaidanieltabuchi.wixsite.com/xyzcoder";
+}
+
+function youtube() {
+	window.location.href = "https://youtube.com/channel/UC92UEMwg9Wf4Oe5_D9C0Xcg";
+}
+
+function news() {
+	window.location.href = "news.html";
+}
+
+function home() {
+	window.location.href = "index.html";
+}
